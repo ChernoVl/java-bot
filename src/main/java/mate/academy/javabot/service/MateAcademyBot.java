8500db696bdaa51fe45854bd19e0c462aed862b3
vladc <vlad.chrv>
2021-08-08 18:35:46 +0200

    Added name, token and annotation to class

--- src/main/java/mate/academy/javabot/service/MateAcademyBot.java
+++ src/main/java/mate/academy/javabot/service/MateAcademyBot.java
@@ -1,21 +1,23 @@
 package mate.academy.javabot.service;
 
+import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
+@Component
 public class MateAcademyBot extends TelegramLongPollingBot {
     @Override
     public String getBotUsername() {
-        return null;
+        return "recipe_by_1234_bot";
     }
 
     @Override
     public String getBotToken() {
-        return null;
+        return "1920925503:AAFkmqiCn1ANjNc13BoQ5-ZAJ6AZR22PGa4";
     }
 
     @Override
     public void onUpdateReceived(Update update) {
-
+        System.out.println("Message received");
     }
 }
